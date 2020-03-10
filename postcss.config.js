9c16a08588e3b8c95ecb4942869bd4683232e600
Anthony Morris <anthony@amorrissound.com>
2020-03-09 23:34:10 -0700

    Dockerize the app (#2)
    * Add Dockerfile
    
    * Fix issue with docker image
    
    * Add cloudbuild.yaml
    
    * Format cloudbuild.yaml
    
    * Format cloudbuild.yaml... again

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,5 @@
 module.exports = {
   plugins: [
     'tailwindcss',
-    // eslint-disable-next-line no-undef
-    process.env.NODE_ENV === 'production'
-      ? [
-        '@fullhuman/postcss-purgecss',
-        {
-          content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}',
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        },
-      ]
-      : undefined,
-  ],
+  ]
 };
