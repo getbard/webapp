c8ec01e116a7bd476f27e92ee75cefc964025d5f
Anthony Morris <anthony@amorrissound.com>
2020-03-13 13:52:57 -0700

    Add firebase config

---
+++ firebase.config.ts
@@ -0,0 +1,16 @@
+const development = {
+  apiKey: "AIzaSyB_XRW2CD8YP6KyF3b-cEhxYpYaPrc1VFs",
+  authDomain: "bard-stage.firebaseapp.com",
+  databaseURL: "https://bard-stage.firebaseio.com",
+  projectId: "bard-stage",
+  storageBucket: "bard-stage.appspot.com",
+  messagingSenderId: "366044167912",
+  appId: "1:366044167912:web:66cd1372dbedf2383ad35d",
+  measurementId: "G-73PC4V2BN1"
+};
+
+export default {
+  development,
+  test: development,
+  production: development,
+}
