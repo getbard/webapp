34242e2be05a2b769644b98f24920a1124af9f15
Anthony Morris <anthony@amorrissound.com>
2020-03-13 12:59:12 -0700

    Minor styling enhancements

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,9 @@ import DiscoverArticles from '../components/DiscoverArticles';
 const Discover: NextPage = (): React.ReactElement => {
   return (
     <div className="px-5 pt-5">
-      <div className="mb-10">
-        <h1 className="text-4xl font-serif font-bold">
-          Discover
-        </h1>
-      </div>
+      <h1 className="text-4xl font-serif font-bold">
+        Discover
+      </h1>
 
       <DiscoverArticles />
     </div>
