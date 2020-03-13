8353a6d377a7c225802d07566cafc5d693046ddc
Anthony Morris <anthony@amorrissound.com>
2020-03-13 12:28:03 -0700

    Add masonry layout

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,10 +1,17 @@
 import { Article } from '../generated/graphql';
 
 function ArticleCard({ data }: { data: Article }): React.ReactElement {
+  const imageSrc = data.headerImageURL || undefined;
   return (
-    <div>
+    <div className="p-5 m-5 border border-black">
+      {
+        imageSrc
+         ? <img className="mb-5" src ={imageSrc} />
+         : null
+      }
+
       <h1>{data.title}</h1>
-      {data.content}
+      {data.subtitle}
     </div>
   );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 function Nav(): React.ReactElement {
   return (
-    <nav className="px-5 flex flex-row justify-between items-center shadow-sm">
+    <nav className="px-5 flex flex-row justify-between items-center border-b border-gray-200">
       <Link href="/">
         <a className="logo font-extrabold text-3xl text-primary font-serif">
           bard.
