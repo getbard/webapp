b61285504ac4063bb8798d96f93899098d214729
Anthony Morris <anthony@amorrissound.com>
2020-03-15 16:25:11 -0700

    Adjust the spacing of the nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,39 +9,41 @@ function Nav(): React.ReactElement {
   const auth = useAuth();
 
   return (
-    <nav className="px-5 py-1 flex flex-row justify-between items-center border-b border-gray-200">
-      <Link href="/">
-        <a className="logo font-extrabold text-3xl text-primary font-serif">
-          bard.
-        </a>
-      </Link>
+    <nav className="py-1 border-b border-gray-200">
+      <div className="container mx-auto flex flex-row justify-between items-center px-5">
+        <Link href="/">
+          <a className="logo font-extrabold text-3xl text-primary font-serif">
+            bard.
+          </a>
+        </Link>
 
-      <div className="flex items-center">
-        {
-          auth.user
-            ? (
-              <>
-                <ButtonLink href="/write" className="mr-4 py-1">
-                  Write
-                </ButtonLink>
-                
-                <NavMenu />
-              </>
-            )
-            : (
-              <>
-                <Link href="/login">
-                  <a className="mr-4">
-                    Login
-                  </a>
-                </Link>
+        <div className="flex items-center">
+          {
+            auth.user
+              ? (
+                <>
+                  <ButtonLink href="/write" className="mr-4 py-1">
+                    Write
+                  </ButtonLink>
+                  
+                  <NavMenu />
+                </>
+              )
+              : (
+                <>
+                  <Link href="/login">
+                    <a className="mr-4">
+                      Login
+                    </a>
+                  </Link>
 
-                <ButtonLink href="/signup" className="py-1">
-                  Join
-                </ButtonLink>
-              </>
-            )
-        }
+                  <ButtonLink href="/signup" className="py-1">
+                    Join
+                  </ButtonLink>
+                </>
+              )
+          }
+        </div>
       </div>
     </nav>
   );
