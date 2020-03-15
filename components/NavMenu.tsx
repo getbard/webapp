1c62282c7f7eda3f3c42d29334a68bd3b887b526
Anthony Morris <anthony@amorrissound.com>
2020-03-15 15:28:58 -0700

    Add nav menu for user settings

---
+++ components/NavMenu.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { FaCog } from 'react-icons/fa';
+import styled from '@emotion/styled'
+// import Link from 'next/link';
+
+import { useAuth } from '../hooks/useAuth';
+
+const StyledCog = styled(FaCog)`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Menu = styled.div`
+  top: 22px;
+  right: -12px;
+`;
+
+const DisplayMenu = (): React.ReactElement => {
+  const auth = useAuth();
+
+  return (
+    <Menu className="z-20 px-4 bg-white border border-gray-300 absolute rounded-sm shadow-sm flex flex-col">
+      <a className="my-2" onClick={(): Promise<boolean | void> => auth.signOut()}>Logout</a>
+    </Menu>
+  );
+}
+
+const NavMenu = (): React.ReactElement => {
+  const [display, setDisplay] = useState(false);
+
+  return (
+    <div className="flex justify-end relative">
+      <StyledCog onClick={(): void => setDisplay(!display)} />
+
+      {display && <DisplayMenu />}
+    </div>
+  );
+}
+
+export default NavMenu;
