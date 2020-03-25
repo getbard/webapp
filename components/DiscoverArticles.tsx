0cb8b9ebfe0840acc8ed6641e127c9d309f43774
Anthony Morris <anthony@amorrissound.com>
2020-03-25 10:52:57 -0700

    Separate articles without images (#28)
    * Make the discover page more dynamic
    
    * Minor style changes
    
    * Add spot for photo credit

--- components/DiscoverArticles.tsx
+++ components/DiscoverArticles.tsx
@@ -1,26 +1,99 @@
 import { useQuery } from '@apollo/react-hooks';
 import { XMasonry, XBlock } from 'react-xmasonry';
+import styled from '@emotion/styled';
+
+import useMedia from '../hooks/useMedia';
 
 import { Article } from '../generated/graphql';
 import DiscoverArticlesQuery from '../queries/DiscoverArticlesQuery';
 
 import ArticleCard from './ArticleCard';
+import SmallArticleCard from './SmallArticleCard';
+
+function shuffle(arr: any[]): any[] {
+  const a = [...arr];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+const ArticleChunkContainer = styled.div`
+  > div:not(:last-child) {
+    border-bottom: 1px solid;
+    border-color: #e0e0e0;
+  }
+`;
 
 function DiscoverArticles(): React.ReactElement {
+  const smallArticleCardWidth = useMedia(['(max-width: 640px)', '@media (min-width: 640px)'], [1, 2], 2);
+  const smallArticleCardCount = useMedia(['(max-width: 640px)', '@media (min-width: 640px)'], [4, 8], 8);
   const { loading, error, data } = useQuery(DiscoverArticlesQuery);
 
   if (error) return <div>Error</div>;
   if (loading) return <div>Loading</div>;
-  
+
+  const { articles } = data;
+  const articleChunks: Article[][] = [];
+  const articlesWithoutHeader: Article[] = [];
+  const articlesWithHeader: Article[] = [];
+
+  articles.forEach((article: Article) => {
+    if (article.headerImageURL) {
+      articlesWithHeader.push(article);
+    } else {
+      articlesWithoutHeader.push(article);
+    }
+  });
+
+  for (let i = 0; i < articlesWithHeader.length; i+= smallArticleCardCount) {
+    articleChunks.push(articlesWithoutHeader.slice(i, i + smallArticleCardCount));
+  }
+
+  const articleBlocks = articlesWithHeader.map((article: Article) => {
+    return (
+      <XBlock key={article.id}>
+        <ArticleCard article={article} />
+      </XBlock>
+    );
+  });
+
+  const articleChunkBlocks = articleChunks.map((articleChunk: Article[], index: number) => {
+    return (
+      <XBlock key={index} width={smallArticleCardWidth}>
+        <div className="grid sm:grid-cols-2 border border-gray-300 m-2 p-1">
+          <ArticleChunkContainer className="sm:hidden">
+            {articleChunk.map((article: Article) => {
+              return (
+                <SmallArticleCard key={article.id} article={article} />
+              );
+            })}
+          </ArticleChunkContainer>
+          <ArticleChunkContainer className="col-span-1 border-r border-gray-300 hidden sm:block">
+            {articleChunk.slice(0, articleChunk.length / 2).map((article: Article) => {
+              return (
+                <SmallArticleCard key={article.id} article={article} />
+              );
+            })}
+          </ArticleChunkContainer>
+          <ArticleChunkContainer className="col-span-1 hidden sm:block">
+            {articleChunk.slice(articleChunk.length / 2).map((article: Article) => {
+              return (
+                <SmallArticleCard key={article.id} article={article} />
+              );
+            })}
+          </ArticleChunkContainer>
+        </div>
+      </XBlock>
+    )
+  });
+
+  const blocks = shuffle([...articleBlocks, ...articleChunkBlocks]);
+
   return (
     <XMasonry>
-      {data.articles.map((article: Article) => {
-        return (
-          <XBlock key={article.id}>
-            <ArticleCard article={article} />
-          </XBlock>
-        );
-      })}
+      {blocks}
     </XMasonry>
   );
 }
--- components/SmallArticleCard.tsx
+++ components/SmallArticleCard.tsx
@@ -6,40 +6,26 @@ import { Article } from '../generated/graphql';
 
 const ArticleCardDiv = styled.div`
   &:hover {
-    border-color: #bdbdbd;
-
     h1 {
       color: #616161;
     }
   }
 `;
 
-type ArticleCardImageProps = {
-  url: string;
-}
-
-const ArticleCardImage = styled.div`
-  background-size: cover;
-  background-position: center;
-  background-image: url(${(props: ArticleCardImageProps): string => props.url});
-`;
-
-function ArticleCard({ article }: { article: Article }): React.ReactElement {
+function SmallArticleCard({ article }: { article: Article }): React.ReactElement {
   const articleHref = article?.slug ? `/articles/s/${article.slug}` : `/articles/i/${article.id}`;
   const authorName = `${article.author.firstName}${article.author?.lastName && ' ' + article.author.lastName[0] + '.'}`;
-  
+
   return (
     <Link href={articleHref}>
-      <ArticleCardDiv className="p-4 m-2 border border-gray-300 rounded-sm hover:cursor-pointer transition duration-150 ease-in">
-        {article?.headerImageURL && <ArticleCardImage className="h-40 rounded-t-sm -mt-3 -mx-3 mb-3 transition duration-150 ease-in" url={article.headerImageURL} />}
-
-        <h1 className="font-serif font-bold text-xl transition duration-150 ease-in">{article.title}</h1>
-        <div className="text-gray-600 text-sm">{article.summary}</div>
+      <ArticleCardDiv className="py-4 px-2 mx-2 rounded-sm hover:cursor-pointer transition duration-150 ease-in">
+        <h1 className="font-serif font-bold text-lg transition duration-150 ease-in">{article.title}</h1>
+        {article.summary && <div className="text-gray-600 mt-1">{article.summary}</div>}
         
         <div className="flex justify-between text-xs align-center font-bold mt-2">
           <div className="text-gray-700">
             {authorName}
-          </div> 
+          </div>  
           {
             article.publishedAt
               ? (
@@ -55,4 +41,4 @@ function ArticleCard({ article }: { article: Article }): React.ReactElement {
   );
 }
 
-export default ArticleCard;
+export default SmallArticleCard;
