1fc2847ff805605e40c6cd50524ea027f3e0fef3
Anthony Morris <anthony@amorrissound.com>
2020-03-26 11:54:41 -0700

    Protect routes

--- components/withLayout.tsx
+++ components/withLayout.tsx
@@ -1,14 +1,32 @@
+import { useEffect } from 'react';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import cookie from 'js-cookie';
+
+const protectedRoutes = [
+  '/write',
+  '/edit/[id]',
+  '/articles',
+];
 
 import Nav from './Nav';
 
 function withLayout(PageComponent: NextPage): NextPage {
   const PageComponentWithLayout = (): React.ReactElement => {
+    const router = useRouter();
+    const isAuthenticated = cookie.get('token') && protectedRoutes.includes(router.pathname);
+
+    useEffect(() => {
+      if (!cookie.get('token') && protectedRoutes.includes(router.pathname)) {
+        router.push('/');
+      }
+    }, []);
+
     return (
       <div>
         <Nav />
   
-        <PageComponent />
+        {(isAuthenticated || !protectedRoutes.includes(router.pathname)) && <PageComponent />}
 
         <div className="pt-5"></div>
       </div>
