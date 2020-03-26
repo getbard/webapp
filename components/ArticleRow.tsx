33791c76ca7b5831b5baec701af7322f736ac5c4
Anthony Morris <anthony@amorrissound.com>
2020-03-26 12:52:46 -0700

    Fix permission issues (#43)
    * Conditionally display article actions
    
    * Protect routes
    
    * Redirect to homepage on logout
    
    * Redirect to the login page when auth is required

--- components/ArticleRow.tsx
+++ components/ArticleRow.tsx
@@ -4,12 +4,16 @@ import Link from 'next/link';
 import { formatDistanceToNow } from 'date-fns';
 import { FiCopy } from 'react-icons/fi';
 
+import { useAuth } from '../hooks/useAuth';
+
 import { Article } from '../generated/graphql';
 
 import Tooltip from './Tooltip';
 
 function ArticleRow({ article }: { article: Article }): React.ReactElement {
+  const auth = useAuth();
   const [showCopiedTooltip, setShowCopiedTooltip] = useState(false);
+  const articleOwner = auth.user?.uid === article.userId;
 
   return (
     <div className="border-b border-gray-200 my-2 py-4 flex justify-between items-center">
@@ -42,37 +46,39 @@ function ArticleRow({ article }: { article: Article }): React.ReactElement {
         </div>
       </div>
 
-      <div className="flex justify-end items-center">
-        {
-          !article?.draft && article?.slug &&
-          (
-            <span
-              id={`${article.id}-copy`} 
-              className="relative"
-              onClick={(): void => {
-                navigator.clipboard.writeText(`https://getbard.com/articles/s/${article.slug}`);
-                setShowCopiedTooltip(true);
-                setTimeout(() => setShowCopiedTooltip(false), 2500);
-              }}
-            >
-              <FiCopy className="block hover:cursor-pointer hover:text-primary mr-4 transition duration-150 ease-in-out" />
-              <Tooltip showTooltip={showCopiedTooltip} selector={`#${article.id}-copy`}>
-                A link to the article has been <br/>
-                copied to your clipboard.
-              </Tooltip>
-            </span>
-          )
-        }
+      {articleOwner && (
+        <div className="flex justify-end items-center">
+          {
+            !article?.draft && article?.slug &&
+            (
+              <span
+                id={`${article.id}-copy`} 
+                className="relative"
+                onClick={(): void => {
+                  navigator.clipboard.writeText(`https://getbard.com/articles/s/${article.slug}`);
+                  setShowCopiedTooltip(true);
+                  setTimeout(() => setShowCopiedTooltip(false), 2500);
+                }}
+              >
+                <FiCopy className="block hover:cursor-pointer hover:text-primary mr-4 transition duration-150 ease-in-out" />
+                <Tooltip showTooltip={showCopiedTooltip} selector={`#${article.id}-copy`}>
+                  A link to the article has been <br/>
+                  copied to your clipboard.
+                </Tooltip>
+              </span>
+            )
+          }
 
-        <Link href={`/edit/${article.id}`}>
-          <a className="inline hover:text-primary hover:cursor-pointer font-medium mr-4 transition duration-150 ease-in-out">
-            Edit
-          </a>
-        </Link>
+          <Link href={`/edit/${article.id}`}>
+            <a className="inline hover:text-primary hover:cursor-pointer font-medium mr-4 transition duration-150 ease-in-out">
+              Edit
+            </a>
+          </Link>
 
-        <div className="inline text-red-600 hover:text-red-900 hover:cursor-pointer font-medium transition duration-150 ease-in-out">Delete</div>
+          <div className="inline text-red-600 hover:text-red-900 hover:cursor-pointer font-medium transition duration-150 ease-in-out">Delete</div>
+        </div>
+      )}
       </div>
-    </div>
   );
 }
 
--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -1,6 +1,9 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useQuery } from '@apollo/react-hooks';
+import Error from 'next/error';
+
+import { useAuth } from '../../hooks/useAuth';
 
 import ArticleByIdQuery from '../../queries/ArticleByIdQuery';
 
@@ -10,13 +13,18 @@ import EditorContainer from '../../components/EditorContainer';
 
 const Edit: NextPage = (): React.ReactElement => {
   const router = useRouter();
+  const auth = useAuth();
   const { id } = router.query;
   const { loading, error, data } = useQuery(ArticleByIdQuery, { variables: { id } });
 
   if (error) return <div>Error</div>;
-  if (loading) return <div>Loading</div>;
+  if (loading || !auth?.user?.uid) return <div>Loading</div>;
 
   const { article } = data;
+
+  if (auth?.user?.uid !== article.userId) {
+    return <Error statusCode={404} />;
+  }
 
   return (
     <EditorContainer article={article} />
