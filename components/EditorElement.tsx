fd1e3728f05e3d53d54c4062b72e5460c2b40432
Anthony Morris <anthony@amorrissound.com>
2020-04-01 15:33:30 -0700

    Allow pasting HTML (#51)
    * Enable pasting HTML
    
    * Trim some line breaks

---
+++ components/EditorElement.tsx
@@ -0,0 +1,55 @@
+import { RenderElementProps } from 'slate-react';
+
+const ImageElement = ({ attributes, children, element }: RenderElementProps): React.ReactElement => {
+  return (
+    <div {...attributes}>
+      {children}
+      <img src={element.url} />
+    </div>
+  );
+};
+
+const EditorElement = (props: RenderElementProps): React.ReactElement => {
+  const { attributes, children, element } = props;
+
+  switch (element.type) {
+    default:
+      return <p {...attributes}>{children}</p>;
+    case 'quote':
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case 'code':
+      return (
+        <pre>
+          <code {...attributes}>{children}</code>;
+        </pre>
+      );
+    case 'bulleted-list':
+      return <ul {...attributes}>{children}</ul>;
+    case 'heading-one':
+      return <h1 {...attributes}>{children}</h1>;
+    case 'heading-two':
+      return <h2 {...attributes}>{children}</h2>;
+    case 'heading-three':
+      return <h3 {...attributes}>{children}</h3>;
+    case 'heading-four':
+      return <h4 {...attributes}>{children}</h4>;
+    case 'heading-five':
+      return <h5 {...attributes}>{children}</h5>;
+    case 'heading-six':
+      return <h6 {...attributes}>{children}</h6>;
+    case 'list-item':
+      return <li {...attributes}>{children}</li>;
+    case 'numbered-list':
+      return <ol {...attributes}>{children}</ol>;
+    case 'link':
+      return (
+        <a href={element.url} {...attributes}>
+          {children}
+        </a>
+      );
+    case 'image':
+      return <ImageElement {...props} />;
+  }
+}
+
+export default EditorElement;
