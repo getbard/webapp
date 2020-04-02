0da3ceff9acd776fd40e493cc3fbed923a96def7
Anthony Morris <anthony@amorrissound.com>
2020-04-02 05:13:31 -0700

    Remove article progress bar 🥺

--- pages/articles/[...id].tsx
+++ pages/articles/[...id].tsx
@@ -1,13 +1,9 @@
-import { useState, useEffect, useRef } from 'react';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useQuery } from '@apollo/react-hooks';
 import { format } from 'date-fns';
 import styled from '@emotion/styled';
-import throttle from 'lodash/throttle';
-
-import useOnScreen from '../../hooks/useOnScreen';
 
 import ArticleBySlugQuery from '../../queries/ArticleBySlugQuery';
 import ArticleByIdQuery from '../../queries/ArticleByIdQuery';
@@ -21,24 +17,6 @@ import Button from '../../components/Button';
 const GradientBlocker = styled.div`
   width: 100%;
   background: linear-gradient(0deg, rgba(255,255,255,1) 20%, rgba(255,255,255,0) 100%);
-`;
-
-type ProgressBarProps = {
-  onScreen: boolean;
-}
-
-const ProgressBar = styled.div`
-  width: 0.15rem;
-  height: 100vh;
-  opacity: ${(props: ProgressBarProps): number => props.onScreen ? 100 : 0};
-`;
-
-type InnerProgressBarProps = {
-  height: number;
-}
-
-const InnerProgressBar = styled.div`
-  height: ${(props: InnerProgressBarProps): number => props.height}%;
 `;
 
 const ContentBlocker = ({ author }: { author: string }): React.ReactElement => {
@@ -68,30 +46,11 @@ const Article: NextPage = (): React.ReactElement => {
   const { id: idParams } = router.query;
   const [idType, id] = idParams;
   const articleQuery = idType === 's' ? ArticleBySlugQuery : ArticleByIdQuery;
-  const [progress, setProgress] = useState(0);
-  const progressRef = useRef<HTMLDivElement>(null);
-  const progressOnScreen = useOnScreen(progressRef, '-200px');
 
   const { loading, error, data } = useQuery(articleQuery, { variables: { id } });
 
   if (error) return <div>Error</div>;
   if (loading) return <div>Loading</div>;
-
-  // Handle updating the progress bar
-  useEffect(() => {
-    const handleScroll = throttle(() => {
-      const el = progressRef.current;
-      const height = el?.offsetHeight || 0;
-      const percentComplete = +(window.scrollY / (document.body.clientHeight - height)).toFixed(2) * 120;
-      setProgress(percentComplete >= 100 ? 100 : percentComplete);
-    }, 20);
-
-    window.addEventListener('scroll', handleScroll);
-
-    return (): void => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   const article = data?.article || data?.articleBySlug;
   const authorName = `${article.author.firstName}${article.author?.lastName && ' ' + article.author.lastName}`;
@@ -125,15 +84,6 @@ const Article: NextPage = (): React.ReactElement => {
       </div>
 
       <div className="relative">
-        <div className="absolute top-0 bottom-0 left-0 -ml-5">
-          <ProgressBar className="sticky top-0 py-20 transition duration-300 ease-in" onScreen={progressOnScreen}>
-            <div className="w-full h-full bg-gray-300" ref={progressRef}>
-              <InnerProgressBar className="bg-primary w-full transition duration-150 ease-in" height={progress} />
-            </div>
-          </ProgressBar>
-        </div>
-
-
         <Editor
           readOnly
           initialValue={JSON.parse(article.content)}
