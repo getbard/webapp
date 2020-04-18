b4ca978d1923e8f9e43a8de40727f61435cb2ebd
Anthony Morris <anthony@amorrissound.com>
2020-04-18 15:55:11 -0700

    Remove quotes from generic loader

--- components/GenericLoader.tsx
+++ components/GenericLoader.tsx
@@ -1,11 +1,6 @@
 import styled from '@emotion/styled';
 import { FiFeather } from 'react-icons/fi';
 
-const quotes = [{
-  text: `Substitute 'damn' every time you're inclined to write 'very'; your editor will delete it and the writing will be just as it should be.`,
-  author: 'Mark Twain',
-}];
-
 const Feather = styled(FiFeather)`
   @keyframes colorChange {
     0% {
@@ -25,23 +20,9 @@ const Feather = styled(FiFeather)`
 
 
 function GenericLoader(): React.ReactElement {
-  const quote = quotes[Math.floor(Math.random()) % quotes.length];
-
   return (
-    <div className="flex justify-center items-center flex-col p-40 text-lg">
-      <div className="px-40 mb-8 font-serif">
-        <div className="text-2xl text-justify">
-          &quot;{quote.text}&quot;
-        </div>
-
-        <div className="text-right">
-          ~ {quote.author}
-        </div>
-      </div>
-
-      <div className="text-4xl font-serif text-primary">
-        <Feather />
-      </div>
+    <div className="flex justify-center items-center h-screen -mt-20 text-6xl">
+      <Feather />
     </div>
   );
 }
