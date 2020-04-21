4de67635e1d77c7c4f675de51cf86fabe35eaf4f
Anthony Morris <anthony@amorrissound.com>
2020-04-21 16:04:10 -0700

    Handle article categories better

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useQuery } from '@apollo/react-hooks';
 
 import { Category, Article } from '../generated/graphql';
+import DiscoverArticlesQuery from '../queries/DiscoverArticlesQuery';
 
 import { useAuth } from '../hooks/useAuth';
 
@@ -10,20 +12,101 @@ import { withApollo } from '../lib/apollo';
 import withLayout from '../components/withLayout';
 import DiscoverArticles from '../components/DiscoverArticles';
 import Feed from '../components/Feed';
+import GenericError from '../components/GenericError';
+import DiscoverArticlesFallback from '../components/DiscoverArticlesFallback';
+
+const defaultCategories = ['all'];
+for (const category in Category) {
+  if (defaultCategories.length !== 11) {
+    defaultCategories.push(category);
+  }
+}
+
+function ArticlesContainer({
+  articles = [],
+  error,
+  loading,
+  category,
+}: {
+  articles: Article[];
+  error: any;
+  loading: boolean;
+  category: string;
+}): React.ReactElement {
+  if (error) return <div><GenericError title /></div>
+  if (loading) return <DiscoverArticlesFallback />;
+
+  return (
+    <DiscoverArticles
+      articles={articles}
+      category={category}
+    />
+  );
+}
 
 const Discover: NextPage = (): React.ReactElement => {
   const auth = useAuth();
   const router = useRouter();
   const [selectedCategory, setSelectedCategory] = useState(router?.query?.category as string || 'all');
+  const [hasSetCategories, setHasSetCategories] = useState(false);
+  const [categories, setCategories] = useState<string[]>(defaultCategories);
+  const { loading, data, error, refetch } = useQuery(DiscoverArticlesQuery, {
+    variables: { category: selectedCategory },
+  });
 
-  const categories = ['all'];
-  for (const category in Category) {
-    if (categories.length !== 11) {
-      categories.push(category);
+  useEffect(() => {
+    refetch({
+      category: selectedCategory,
+    });
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    if (hasSetCategories) {
+      return;
     }
-  }
 
-  if (auth.userId) {
+    const articles = data?.articles || [];
+  
+    const categoryCounter = new Map();
+  
+    articles.forEach((article: Article) => {
+      if (article.category) {
+        const category = article.category;
+
+        if (categoryCounter.has(category)) {
+          categoryCounter.set(category, categoryCounter.get(category));
+        } else if (categoryCounter.size !== 10) {
+          categoryCounter.set(category, 1);
+        }
+      }
+    });
+
+    for (const category in Category) {
+      if (!categoryCounter.has(category.toLowerCase()) && categoryCounter.size !== 10) {
+        categoryCounter.set(category.toLowerCase(), 0);
+      }
+    }
+  
+    const newCategories = [...categoryCounter.keys()];
+    newCategories.sort((a, b) => {
+      if (a > b) {
+        return 1;
+      }
+
+      if (a < b) {
+        return -1;
+      }
+
+      return 0;
+    });
+  
+    newCategories.unshift('all');
+
+    setCategories(newCategories);
+    setHasSetCategories(true);
+  }, [data?.articles.length]);
+
+  if (auth.userId && categories[0] !== 'feed') {
     categories.unshift('feed');
   }
 
@@ -58,7 +141,10 @@ const Discover: NextPage = (): React.ReactElement => {
         selectedCategory === 'feed'
           ? <Feed />
           : (
-            <DiscoverArticles
+            <ArticlesContainer
+              error={error}
+              loading={loading}
+              articles={data?.articles}
               category={selectedCategory}
             />
           )
