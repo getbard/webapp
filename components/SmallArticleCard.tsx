0ce89da6179ba4f09717373b9dfd9fa9511cf30f
Anthony Morris <anthony@amorrissound.com>
2020-04-24 14:09:25 -0700

    Refactor article cards to enable clicking author (#160)

--- components/SmallArticleCard.tsx
+++ components/SmallArticleCard.tsx
@@ -9,10 +9,6 @@ import { timeToRead } from '../lib/editor';
 
 const ArticleCardContainer = styled.div`
   &:hover {
-    h1 {
-      color: #616161;
-    }
-
     .sub-only {
       display: flex;
     }
@@ -32,82 +28,95 @@ function SmallArticleCard({ article }: { article: Article }): React.ReactElement
   const authorName = `${article.author.firstName}${article.author?.lastName && ' ' + article.author.lastName[0] + '.'}`;
   const readingTime = timeToRead(article.wordCount);
 
+  const trackingData = {
+    article: {
+      id: article.id,
+      title: article.title,
+      slug: article.slug,
+      readingTime,
+      subscribersOnly: article.subscribersOnly,
+      category: article.category,
+    },
+    author: {
+      id: article.author.id,
+    }
+  };
+
   const handleClick = (): void => {
-    window.analytics.track(`SMALL ARTICLE CARD: clicked`, {
-      article: {
-        id: article.id,
-        title: article.title,
-        slug: article.slug,
-        readingTime,
-        subscribersOnly: article.subscribersOnly,
-        category: article.category,
-      },
-      author: {
-        id: article.author.id,
-      }
-    });
+    window.analytics.track(`SMALL ARTICLE CARD: clicked`, trackingData);
+  }
+
+  const handleAuthorClick = (): void => {
+    window.analytics.track(`SMALL ARTICLE CARD: author name clicked`, trackingData);
   }
 
   return (
-    <Link href="/articles/[...id]" as={articleHref} passHref={true}>
-      <ArticleCardContainer
-        className="pt-1 pb-2 px-2 rounded-sm hover:cursor-pointer transition duration-150 ease-in"
-        onClick={handleClick}
-      >
-        <div className={`${article.subscribersOnly && 'grid grid-cols-8'}`}>
-          <h1 className="col-span-7 font-serif font-bold transition duration-150 ease-in">
+    <ArticleCardContainer
+      className="pt-1 pb-2 px-2 rounded-sm transition duration-150 ease-in"
+      onClick={handleClick}
+    >
+      <Link href="/articles/[...id]" as={articleHref} passHref={true}>
+        <a>
+          <div className={`${article.subscribersOnly && 'grid grid-cols-8'}`}>
+            <h1 className="col-span-7 font-serif font-bold transition duration-150 ease-in">
+              {
+                article.title.length > 45
+                  ? `${article.title.substr(0, article.title.lastIndexOf(' ', 45))}...`
+                  : article.title
+              }
+            </h1>
+
             {
-              article.title.length > 45
-                ? `${article.title.substr(0, article.title.lastIndexOf(' ', 45))}...`
-                : article.title
+              article.subscribersOnly && (
+                <SubOnlyIcon className="sub-only hidden items-center text-primary">
+                  <FiFeather />
+                </SubOnlyIcon>
+              )
             }
-          </h1>
+          </div>
+
 
           {
-            article.subscribersOnly && (
-              <SubOnlyIcon className="sub-only hidden items-center text-primary">
-                <FiFeather />
-              </SubOnlyIcon>
+            article.summary && (
+              <div className="text-gray-600 mt-1 text-sm">
+                {
+                  article.summary.length > 100
+                    ? `${article.summary.substr(0, article.summary.lastIndexOf(' ', 100))}...`
+                    : article.summary
+                }
+              </div>
             )
           }
+        </a>
+      </Link>
+
+      <div className="text-xs mt-2  font-medium flex justify-between">
+        <Link href={`/${article.author.username}`} >
+          <a
+            className="text-gray-700"
+            onClick={handleAuthorClick}
+          >
+            {authorName}
+          </a>
+        </Link>
+
+        <div>
+          {
+            article.publishedAt
+              ? (
+                <span className="text-gray-700">
+                  {format(new Date(article.publishedAt), 'LLL d')} |&nbsp;
+                </span>
+              )
+              : ''
+          }
+
+          <span className="time-to-read">
+            {readingTime}
+          </span>
         </div>
-        
-
-        {
-          article.summary && (
-            <div className="text-gray-600 mt-1 text-sm">
-              {
-                article.summary.length > 100
-                  ? `${article.summary.substr(0, article.summary.lastIndexOf(' ', 100))}...`
-                  : article.summary
-              }
-            </div>
-          )
-        }
-        
-        <div className="text-xs mt-2  font-medium flex justify-between">
-            <div className="text-gray-700">
-              {authorName}
-            </div> 
-
-            <div>
-              {
-                article.publishedAt
-                  ? (
-                    <span className="text-gray-700">
-                      {format(new Date(article.publishedAt), 'LLL d')} |&nbsp;
-                    </span>
-                  )
-                  : ''
-              }
-
-              <span className="time-to-read">
-                {readingTime}
-              </span>
-            </div>
-          </div>
-      </ArticleCardContainer>
-    </Link>
+      </div>
+    </ArticleCardContainer>
   );
 }
 
