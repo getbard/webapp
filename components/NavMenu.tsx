99806c6767f1331c7fd23e0408ee0a0a33abfa19
Anthony Morris <anthony@amorrissound.com>
2020-04-29 08:16:26 -0700

    Dynamically get the username for the profile link

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,8 +1,10 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { FiSettings } from 'react-icons/fi';
 import styled from '@emotion/styled'
 import Link from 'next/link';
 import { useApolloClient } from '@apollo/react-hooks';
+
+import UsernameQuery from '../queries/UsernameQuery';
 
 import { useAuth } from '../hooks/useAuth';
 import useOnClickOutside from '../hooks/useOnClickOutside';
@@ -14,6 +16,7 @@ const Menu = styled.div`
 
 function DisplayMenu(): React.ReactElement {
   const auth = useAuth();
+  const [username, setUsername] = useState('me');
 
   let apolloClient: any;
   try {
@@ -23,6 +26,23 @@ function DisplayMenu(): React.ReactElement {
     // The cache will not get cleared if a user logs out
     // This only happens during error states
   }
+
+  useEffect(() => {
+    if (apolloClient) {
+      apolloClient.query({
+        query: UsernameQuery,
+        variables: {
+          username: 'me',
+        },
+      }).then(({ data }: any) => {
+        const newUsername = data?.user?.username || 'me';
+
+        if (newUsername !== username) {
+          setUsername(newUsername);
+        }
+      });
+    }
+  }, [apolloClient]);
   
   const handleLogout = (): void => {
     window.analytics.track('MENU: Logout clicked');
@@ -66,7 +86,7 @@ function DisplayMenu(): React.ReactElement {
 
       <div className="border-b border-gray-200"></div>
 
-      <Link href="/me">
+      <Link href={`/${username}`}>
         <a
           className="hover:bg-gray-100 block py-2 pl-2 pr-12"
           onClick={(): void => window.analytics.track('MENU: Profile clicked')}
