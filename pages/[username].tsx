4cba193d224a754561f1106ac509d74203ce468d
Anthony Morris <anthony@amorrissound.com>
2020-04-30 15:55:13 -0700

    🤦🏻‍♂️🤦🏻‍♂️

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -201,18 +201,20 @@ const Author: NextPage = (): React.ReactElement => {
               />
 
               {
-                user?.profileSections?.length && (
-                  <div className="inline whitespace-no-wrap space-x-8">
-                    {user.profileSections.map(profileSection => (
-                      <ProfileSectionSelector
-                        key={profileSection?.id}
-                        name={profileSection?.title || ''}
-                        setSection={setSection}
-                        section={section.toLowerCase()}
-                      />
-                    ))}
-                  </div>
-                )
+                user?.profileSections?.length
+                  ? (
+                    <div className="inline whitespace-no-wrap space-x-8">
+                      {user.profileSections.map(profileSection => (
+                        <ProfileSectionSelector
+                          key={profileSection?.id}
+                          name={profileSection?.title || ''}
+                          setSection={setSection}
+                          section={section.toLowerCase()}
+                        />
+                      ))}
+                    </div>
+                  )
+                  : ''
               }
 
               {
