bd400608b0578a1319a98e81787653b6cbbe2e8d
Anthony Morris <anthony@amorrissound.com>
2020-05-01 09:53:14 -0700

    Setup testing environment

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+    '/coverage/',
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
+};
