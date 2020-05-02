bba0b52a0f5b19c1089e260a7b8e5df106c30199
Anthony Morris <anthony@amorrissound.com>
2020-05-02 09:29:18 -0700

    Add PhotoSelectorFallback

---
+++ components/PhotoSelectorFallback.tsx
@@ -0,0 +1,22 @@
+function PhotoSelectorFallback(): React.ReactElement {
+  return (
+    <div>
+      <div className="px-64 mb-2 border border-gray-300 rounded-sm py-2 px-3 h-8 bg-gray-300"></div>
+
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-2">
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+        <div className="col-span-1 h-24 w-40 bg-gray-300"></div>
+      </div>
+    </div>
+  );
+}
+
+export default PhotoSelectorFallback;
