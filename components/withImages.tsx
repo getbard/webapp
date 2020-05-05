fd534cc12fdbc89ecb762c1563de9ba647b4a7cf
Anthony Morris <anthony@amorrissound.com>
2020-05-05 15:52:35 -0700

    Handle edge cases for image captions

--- components/withImages.tsx
+++ components/withImages.tsx
@@ -2,7 +2,7 @@ import { Node, Transforms, Path, Editor } from 'slate';
 import { ReactEditor } from 'slate-react';
 
 const withImages = (editor: ReactEditor): ReactEditor => {
-  const { normalizeNode, insertBreak } = editor;
+  const { normalizeNode, insertBreak, deleteBackward } = editor;
 
   editor.normalizeNode = ([node, path]: [Node, Path]): void => {
     if (node.type === 'image') {
@@ -15,6 +15,30 @@ const withImages = (editor: ReactEditor): ReactEditor => {
     }
 
     return normalizeNode([node, path]);
+  }
+
+  editor.deleteBackward = (unit: 'character' | 'word' | 'line' | 'block'): void => {
+    const parent = Editor.above(editor, {
+      match: n => ['image', 'caption'].includes(n.type),
+    });
+    
+    if (parent) {
+      const [, path] = parent;
+      const nextPath = Path.next(path);
+
+      try {
+        const [nextNode] = Editor.node(editor, nextPath);
+        if (nextNode?.type === 'caption') {
+          Transforms.removeNodes(editor, { at: nextPath });
+        }
+      } catch (error) {
+        // No node found
+      }
+
+      Transforms.setNodes(editor, { type: 'paragraph' });
+    }
+
+    deleteBackward(unit);
   }
 
   editor.insertBreak = (): void => {
