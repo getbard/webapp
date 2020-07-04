dcf8cf00c3dc2dfe4e8c9173657101e8ff9b9dc0
Anthony Morris <anthony@amorrissound.com>
2020-07-03 20:00:26 -0700

    Fix discover fallback

--- components/DiscoverArticlesFallback.tsx
+++ components/DiscoverArticlesFallback.tsx
@@ -1,35 +1,17 @@
-import styled from '@emotion/styled';
-
 import ArticleCardFallback from './ArticleCardFallback';
-import SmallArticleCardFallback from './SmallArticleCardFallback';
-
-const ArticleChunkContainer = styled.div`
-  > div:not(:first-of-type) {
-    padding-top: 1rem;
-  }
-
-  > div:not(:last-child) {
-    border-bottom: 1px solid;
-    border-color: #f5f5f5;
-  }
-`;
 
 function DiscoverArticles(): React.ReactElement {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 gap-2 md:gap-3">
+    <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-4">
       <ArticleCardFallback />
       <ArticleCardFallback />
       <ArticleCardFallback />
-
-      <ArticleChunkContainer className="row-span-2 col-span-1 border border-gray-200 p-2">
-        <SmallArticleCardFallback />
-        <SmallArticleCardFallback />
-        <SmallArticleCardFallback />
-        <SmallArticleCardFallback />
-        <SmallArticleCardFallback />
-        <SmallArticleCardFallback />
-      </ArticleChunkContainer>
-
+      <ArticleCardFallback />
+      <ArticleCardFallback />
+      <ArticleCardFallback />
+      <ArticleCardFallback />
+      <ArticleCardFallback />
+      <ArticleCardFallback />
       <ArticleCardFallback />
       <ArticleCardFallback />
       <ArticleCardFallback />
