3106b256f114106ddeb94270b9271760250fb867
Anthony Morris <anthony@amorrissound.com>
2020-12-02 18:18:36 -0800

    💣

--- lib/dates.ts
+++
@@ -1,8 +0,0 @@
-import { format, isToday, formatDistanceToNow } from 'date-fns';
-
-export const formatPretty = (dateString: string, min?: boolean): string => {
-  const date = new Date(dateString);
-  return isToday(date)
-    ? `${formatDistanceToNow(date)}${min ? '' : ' ago'}`
-    : `${min ? '' : 'on '}${format(date, 'MMM do, yyyy')}`;
-}
--- queries/AuthLinkQuery.ts
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-const AuthLinkQuery = gql`
-  query authLink($type: AuthLink!, $email: String!) {
-    authLink(type: $type, email: $email)
-  }
-`;
-
-export default AuthLinkQuery;
